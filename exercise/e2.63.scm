141980ebeb03ac90b7cf39be2bc4a3fcfe8fdef8
ck <chengkechengke@gmail.com>
2013-05-21 19:49:52 +0800

    finish exercise 2.64 and 2.65

--- exercise/e2.63.scm
+++ exercise/e2.63.scm
@@ -38,3 +38,63 @@
 ;;  ref http://wiki.drewhess.com/wiki/SICP_exercise_2.63
 
 
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+	(let ((left-result (partial-tree elts left-size)))
+	  (let ((left-tree (car left-result))
+		(non-left-elts (cdr left-result))
+		(right-size (- n (+ left-size 1))))
+	    (let ((this-entry (car non-left-elts))
+		  (right-result (partial-tree (cdr non-left-elts)
+					      right-size)))
+	      (let ((right-tree (car right-result))
+		    (remaining-elts (cdr right-result)))
+		(cons (make-tree this-entry left-tree right-tree)
+		      remaining-elts))))))))
+
+;; test cse
+(define lst (list 1 3 5 7 9 11))
+(define tre (list->tree lst))
+
+;;; exercise 2.65
+
+(define (union-list s1 s2)
+  (cond ((or (null? s1) (null? s2)) (append s1 s2))
+	((= (car s1) (car s2)) 
+	 (cons (car s1) (union-set (cdr s1) (cdr s2))))
+	((> (car s1) (car s2))
+	 (cons (car s2) (union-set s1 (cdr s2))))
+	((< (car s1) (car s2)) 
+	 (cons (car s1) (union-set (cdr s1) s2)))))
+
+(define (intersection-list set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+	(cond ((= x1 x2)
+	       (cons x1
+		     (intersection-list (cdr set1)
+				       (cdr set2))))
+	      ((< x1 x2)
+	       (intersection-list (cdr set1) set2))
+	      ((< x2 x1)
+	       (intersection-list set1 (cdr set2)))))))
+
+
+(define (union-set s1 s2)
+  (let ((lst1 (tree->list-2 s1))
+	(lst2 (tree->list-2 s2)))
+    (let ((lst (union-list lst1 lst2)))
+      (list->tree lst))))
+
+(define (intersection-set s1 s2)
+  (let ((lst1 (tree->list-2 s1))
+	(lst2 (tree->list-2 s2)))
+    (let ((lst (intersection-list lst1 lst2)))
+      (list->tree lst))))
+
