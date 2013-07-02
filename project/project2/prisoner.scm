28dfd37165baf5abff4ad5763cd464accbdc40ff
ck <chengkechengke@gmail.com>
2013-07-02 19:29:29 +0800

    finish project 2, problem 1

---
+++ project/project2/prisoner.scm
@@ -0,0 +1,131 @@
+
+
+(define *game-association-list*
+  ;; format is that first sublist identifies the players' choices 
+  ;; with "c" for cooperate and "d" for defect; and that second sublist 
+  ;; specifies payout for each player
+  '((("c" "c") (3 3))
+    (("c" "d") (0 5))
+    (("d" "c") (5 0))
+    (("d" "d") (1 1))))
+
+(define (get-point-list game)
+  (cadr (extract-entry game *game-association-list*)))
+
+;; note that you will need to write extract-entry
+
+(define make-play list)
+
+(define the-empty-history '())
+
+(define extend-history cons)
+(define empty-history? null?)
+
+(define most-recent-play car)
+(define rest-of-plays cdr)
+
+;; A sampler of strategies
+
+(define (NASTY my-history other-history)
+  "d")
+
+(define (PATSY my-history other-history)
+  "c")
+
+(define (SPASTIC my-history other-history)
+  (if (= (random 2) 0)
+      "c"
+      "d"))
+
+(define (EGALITARIAN  my-history other-history)
+  (define (count-instances-of test hist)
+    (cond ((empty-history? hist) 0)
+	  ((string=? (most-recent-play hist) test)
+	   (+ (count-instances-of test (rest-of-plays hist)) 1))
+	  (else (count-instances-of test (rest-of-plays hist)))))
+  (let ((ds (count-instances-of "d" other-history))
+	(cs (count-instances-of "c" other-history)))
+    (if (> ds cs) "d" "c")))
+
+(define (EYE-FOR-EYE my-history other-history)
+  (if (empty-history? my-history)
+      "c"
+      (most-recent-play other-history)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; my code
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; problem 1
+(define (equal-play? play1 play2)
+  (cond ((and (null? play1) (null? play2)) true)
+	((and (not (null? play1)) (not (null? play2))) 
+	 (if (string=? (car play1) (car play2))
+	     (equal-play? (cdr play1) (cdr play2))
+	     false))
+	(else false)))
+
+(define (extract-entry game game-association-list)
+  (cond ((null? game-association-list)
+	 (error "cannot find game in the list"))
+	((equal-play? game (car (car game-association-list))) 
+	 (car game-association-list))
+	(else (extract-entry game (cdr game-association-list)))))
+  
+;; test cases
+(define a-play (make-play "c" "d"))
+(extract-entry a-play *game-association-list*)
+;Value 14: (("c" "d") (0 5))
+(define a-play (make-play "c" "e"))
+(extract-entry a-play *game-association-list*)
+;cannot find game in the list
+(define a-play (make-play "c" "c"))
+(extract-entry a-play *game-association-list*)
+;Value 15: (("c" "c") (3 3))
+(define a-play (make-play "c"))
+(extract-entry a-play *game-association-list*)
+;cannot find game in the list
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; code to use in 3 player game
+;;	    
+
+;(define *game-association-list*
+;  (list (list (list "c" "c" "c") (list 4 4 4))
+;        (list (list "c" "c" "d") (list 2 2 5))
+;        (list (list "c" "d" "c") (list 2 5 2))
+;        (list (list "d" "c" "c") (list 5 2 2))
+;        (list (list "c" "d" "d") (list 0 3 3))
+;        (list (list "d" "c" "d") (list 3 0 3))
+;        (list (list "d" "d" "c") (list 3 3 0))
+;        (list (list "d" "d" "d") (list 1 1 1))))
+
+
+;; in expected-values: #f = don't care 
+;;                      X = actual-value needs to be #f or X 
+;(define (test-entry expected-values actual-values) 
+;   (cond ((null? expected-values) (null? actual-values)) 
+;         ((null? actual-values) #f) 
+;         ((or (not (car expected-values)) 
+;              (not (car actual-values)) 
+;              (= (car expected-values) (car actual-values))) 
+;          (test-entry (cdr expected-values) (cdr actual-values))) 
+;         (else #f))) 
+;
+;(define (is-he-a-fool? hist0 hist1 hist2) 
+;   (test-entry (list 1 1 1) 
+;               (get-probability-of-c 
+;                (make-history-summary hist0 hist1 hist2))))
+;
+;(define (could-he-be-a-fool? hist0 hist1 hist2)
+;  (test-entry (list 1 1 1)
+;              (map (lambda (elt) 
+;                      (cond ((null? elt) 1)
+;                            ((= elt 1) 1)  
+;                            (else 0)))
+;                   (get-probability-of-c (make-history-summary hist0 
+;                                                               hist1
+;                                                               hist2)))))
+
