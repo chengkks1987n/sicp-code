3c62ff80b8ffcf2c837db729429076f179d6f5d7
cheky <chengkechengke@gmail.com>
2014-04-08 23:36:34 -0400

    start project 5, changed some codes and run it correctly

---
+++ project/project5/meval.scm
@@ -0,0 +1,108 @@
+;;
+;; meval.scm - 6.001 Spring 2005
+;; implementation of meval 
+;;
+
+
+(define (m-eval exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))    
+        ((quoted? exp) (text-of-quotation exp))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((lambda? exp)
+         (make-procedure (lambda-parameters exp) (lambda-body exp) env))
+        ((begin? exp) (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (m-eval (cond->if exp) env))
+	((let? exp) (m-eval (let->application exp) env))
+;	((do-while? exp) (eval-do-while exp env))
+        ((application? exp)
+         (m-apply (m-eval (operator exp) env)
+                (list-of-values (operands exp) env)))
+        (else (error "Unknown expression type -- EVAL" exp))))
+
+(define (m-apply procedure arguments)
+  (cond ((primitive-procedure? procedure)
+         (apply-primitive-procedure procedure arguments))
+        ((compound-procedure? procedure)
+         (eval-sequence
+          (procedure-body procedure)
+          (extend-environment (procedure-parameters procedure)
+                              arguments
+                              (procedure-environment procedure))))
+        (else (error "Unknown procedure type -- APPLY" procedure))))
+
+(define (list-of-values exps env)
+  (cond ((no-operands? exps) '())
+        (else (cons (m-eval (first-operand exps) env)
+                    (list-of-values (rest-operands exps) env)))))
+
+(define (eval-if exp env)
+  (if (m-eval (if-predicate exp) env)
+      (m-eval (if-consequent exp) env)
+      (m-eval (if-alternative exp) env)))
+
+(define (eval-sequence exps env)
+  (cond ((last-exp? exps) (m-eval (first-exp exps) env))
+        (else (m-eval (first-exp exps) env)
+              (eval-sequence (rest-exps exps) env))))
+
+(define (eval-assignment exp env)
+  (set-variable-value! (assignment-variable exp)
+                       (m-eval (assignment-value exp) env)
+                       env))
+
+(define (eval-definition exp env)
+  (define-variable! (definition-variable exp)
+                    (m-eval (definition-value exp) env)
+                    env))
+
+(define (let->application expr)
+  (let ((names (let-bound-variables expr))
+        (values (let-values expr))
+        (body (let-body expr)))
+    (make-application (make-lambda names body)
+		      values)))
+
+(define (cond->if expr)
+  (let ((clauses (cond-clauses expr)))
+    (if (null? clauses)
+	#f
+	(if (eq? (car (first-cond-clause clauses)) 'else)
+	    (make-begin (cdr (first-cond-clause clauses)))
+	    (make-if (car (first-cond-clause clauses))
+		     (make-begin (cdr (first-cond-clause clauses)))
+		     (make-cond (rest-cond-clauses clauses)))))))
+
+(define input-prompt ";;; M-Eval input:")
+(define output-prompt ";;; M-Eval value:")
+
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (do-meval-read)))
+    (if (eq? input '**quit**)
+	'meval-done
+	(let ((output (m-eval input the-global-environment)))
+	  (announce-output output-prompt)
+	  (display output)
+	  (driver-loop)))))
+
+(define (prompt-for-input string)
+  (newline) (newline) (display string) (newline))
+
+(define (announce-output string)
+  (newline) (display string) (newline))
+
+(define *meval-warn-define* #t) ; print warnings?
+(define *in-meval* #t)          ; evaluator running
+
+(define (do-meval-read)
+  (if *in-meval*
+      (read)
+      (dynamic-wind (lambda () (set! *in-meval* #t) (set-meval-mode!))
+		    (lambda () (read))
+		    (lambda () (set! *in-meval* #f) (clear-meval-mode!)))))
+
+;;;;;;;;;;;;;;;;;;; end gift
+
---
+++ project/project5/mycode.scm
@@ -0,0 +1,7 @@
+(load "meval.scm")
+(load "syntax.scm")
+(load "environment.scm")
+
+(driver-loop)
+
+
