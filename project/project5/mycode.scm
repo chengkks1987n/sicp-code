5144b318dd4feb154eec5be3572fc17ba9f6288d
cheky <chengkechengke@gmail.com>
2014-04-09 02:38:58 -0400

    in project5, add some tests for' m-eval'

--- project/project5/mycode.scm
+++ project/project5/mycode.scm
@@ -2,6 +2,54 @@
 (load "syntax.scm")
 (load "environment.scm")
 
-(driver-loop)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; some tests
+(m-eval ''s the-global-environment)
+;Value: s
+(m-eval '12 the-global-environment)
+;Value: 12
 
+(m-eval '(+ 1 2) the-global-environment)
+;Value: 3
+(m-eval '(- 1 2) the-global-environment)
+;Value: -1
+(m-eval '(/ 1 2) the-global-environment)
+;Unbound variable -- LOOKUP /
+(m-eval '(* 1 2) the-global-environment)
+;Unbound variable -- LOOKUP *
 
+(m-eval '(car '(1 2 3 4)) the-global-environment)
+;Value: 1
+(m-eval '(cdr '(1 2 3 4)) the-global-environment)
+;Value 12: (2 3 4)
+(m-eval '(null? '(1 2 3 4)) the-global-environment)
+;Value: #f
+(m-eval '(null? '()) the-global-environment)
+;Value: #t
+
+(m-eval '(if (> 1 2) 'yes 'no) the-global-environment)
+;Value: no
+
+(m-eval '(begin (define fib 12)
+		(+ fib fib 4))
+	the-global-environment)
+;Value: 28
+(m-eval '(begin (define (fib n)
+		  (cond ((= n 0) n)
+			((= n 1) n)
+			(else (+ (fib (- n 1)) (fib (- n 2))))))
+		(fib 4))
+	the-global-environment)
+;Value: 3
+
+(m-eval '(or 'true 'false) the-global-environment)
+;Unbound variable -- LOOKUP or
+(m-eval '(and 'true 'false) the-global-environment)
+;Unbound variable -- LOOKUP and
+
+(m-eval '(let ((x 10)
+	       (y 12))
+	   (set! x (+ x x))
+	   (+ x y))
+	the-global-environment)
+;Value: 32
