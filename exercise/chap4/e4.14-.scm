20db13a4cbc84bf47bee1c471e46ffb5dd1dff6a
cheky <chengkechengke@gmail.com>
2014-04-16 04:02:56 -0400

    add some more explains for exercise 4.14

--- exercise/chap4/e4.14-.scm
+++ exercise/chap4/e4.14-.scm
@@ -9,9 +9,13 @@
 ; error>
 ;The object (primitive #[compiled-procedure 11 ("list" #x1) #xf #x321823]) is not applicable.
 
+;(m-eval '(pmap car '((a 1) (b 2) (c 3))) the-global-environment) is converted
+; to (apply map '(car '((a 1) (b 2) (c 3)))), the 'car is just a symbol not 
+; a primivate-procedure for scheme. it should be (apply map (list car '((a 1) ..
+
 ;; because the map is a high order procedure, one of the arguments of map is
-; also a procedure!  the procedure argument is applyed to scheme-apply,
-;  not evaluated by self-define apply, so things go wrong.
+; also a procedure!  the procedure argument is applyed to scheme-apply just
+;  as symbol, not evaluated by self-define apply, so things go wrong.
 
 ;;;  self-define map
 (m-eval '(define (my-map pro lst)
