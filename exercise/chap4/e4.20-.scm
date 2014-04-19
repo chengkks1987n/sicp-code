8db591c0db7bd2f528169631cf89e89675d9519b
cheky <chengkechengke@gmail.com>
2014-04-18 22:48:47 -0400

    finish exercise 4.22

--- exercise/chap4/e4.20-.scm
+++ exercise/chap4/e4.20-.scm
@@ -217,3 +217,162 @@
 ;Value: #f
 (f 6)
 ;Value: #t
+
+;;;; evaluator with syntax analysis
+(define (eval exp env)
+  ((analyze exp) env))
+(define (analyze exp)
+  (cond ((self-evaluating? exp)
+	 (analyze-self-evaluating exp))
+	((quoted? exp) (analyze-quoted exp))
+	((variable? exp) (analyze-variable exp))
+	((assignment? exp) (analyze-assignment exp))
+	((definition? exp) (analyze-definition exp))
+	((if? exp) (analyze-if exp))
+	((lambda? exp) (analyze-lambda exp))
+	((begin? exp) (analyze-sequence (begin-actions exp)))
+	((cond? exp) (analyze (cond->if exp)))
+	((application? exp) (analyze-application exp))
+	(else
+	 (error "Unknown expression type -- ANALYZE" exp))))
+(define (analyze-self-evaluating exp)
+  (lambda (env) exp))
+(define (analyze-quoted exp)
+  (let ((qval (text-of-quotation exp)))
+    (lambda (env) qval)))
+(define (analyze-variable exp)
+  (lambda (env) (lookup-variable-value exp env)))
+(define (analyze-assignment exp)
+  (let ((var (assignment-variable exp))
+	(vproc (analyze (assignment-value exp))))
+    (lambda (env)
+      (set-variable-value! var (vproc env) env)
+      'ok)))
+(define (analyze-definition exp)
+  (let ((var (definition-variable exp))
+	(vproc (analyze (definition-value exp))))
+    (lambda (env)
+      (define-variable! var (vproc env) env)
+      'ok)))
+(define (analyze-if exp)
+  (let ((pproc (analyze (if-predicate exp)))
+	(cproc (analyze (if-consequent exp)))
+	(aproc (analyze (if-alternative exp))))
+    (lambda (env)
+      (if (true? (pproc env))
+	  (cproc env)
+	  (aproc env)))))
+(define (analyze-lambda exp)
+  (let ((vars (lambda-parameters exp))
+	(bproc (analyze-sequence (lambda-body exp))))
+    (lambda (env) (make-procedure vars bproc env))))
+(define (analyze-sequence exps)
+  (define (sequentially proc1 proc2)
+    (lambda (env) (proc1 env) (proc2 env)))
+  (define (loop first-proc rest-procs)
+    (if (null? rest-procs)
+	first-proc
+	(loop (sequentially first-proc (car rest-procs))
+	      (cdr rest-procs))))
+  (let ((procs (map analyze exps)))
+    (if (null? procs)
+	(error "Empty sequence -- ANALYZE"))
+    (loop (car procs) (cdr procs))))
+(define (analyze-application exp)
+  (let ((fproc (analyze (operator exp)))
+	(aprocs (map analyze (operands exp))))
+    (lambda (env)
+      (execute-application (fproc env)
+			   (map (lambda (aproc) (aproc env))
+				aprocs)))))
+(define (execute-application proc args)
+  (cond ((primitive-procedure? proc)
+	 (apply-primitive-procedure proc args))
+	((compound-procedure? proc)
+	 ((procedure-body proc)
+	  (extend-environment (procedure-parameters proc)
+			      args
+			      (procedure-environment proc))))
+	(else
+	 (error
+	  "Unknown procedure type -- EXECUTE-APPLICATION"
+	  proc))))
+
+(define (true? x)
+  (not (eq? x false)))
+(define (false? x)
+  (eq? x false))
+
+;; test for evaluator with syntax analysisi
+;;; test self-evaluating
+(eval '12 the-global-environment)
+;Value: 12
+;;; test quote
+(eval ''s the-global-environment)
+;Value: s
+;;; test definition
+(eval '(define x 2) the-global-environment)
+;Value: ok
+;;; test variable
+(eval 'x the-global-environment)
+;Value: 2
+;;; test assignment
+(eval '(set! x 3) the-global-environment)
+;Value: ok
+(eval 'x the-global-environment)
+;Value: 3
+;;; test if
+(eval '(if (> 1 2) 'yes 'no) the-global-environment)
+;Value: no
+;;; test begin and definitions
+(eval '(begin (define fib 12)
+	      (+ fib fib 4))
+      the-global-environment)
+;Value: 28
+;;; test begin, cond and application
+(eval '(begin (define (fib n)
+		(cond ((= n 0) n)
+		      ((= n 1) n)
+		      (else (+ (fib (- n 1)) (fib (- n 2))))))
+	      (fib 4))
+      the-global-environment)
+;Value: 3
+
+;;;; exercise 4.22
+;; these procedures are from exercise 4.6
+(define (let? exp)
+  (tagged-list? exp 'let))
+(define (let-vars exp)
+  (map car (cadr exp)))
+(define (let-exps exp)
+  (map cadr (cadr exp)))
+(define (let-body exp)
+  (cddr exp))
+(define (let->combinations exp)
+  (cons 
+   (make-lambda (let-vars exp) (let-body exp))
+   (let-exps exp))
+  )
+
+(define (analyze exp)
+  (cond ((self-evaluating? exp)
+	 (analyze-self-evaluating exp))
+	((quoted? exp) (analyze-quoted exp))
+	((variable? exp) (analyze-variable exp))
+	((assignment? exp) (analyze-assignment exp))
+	((definition? exp) (analyze-definition exp))
+	((if? exp) (analyze-if exp))
+	((lambda? exp) (analyze-lambda exp))
+	((begin? exp) (analyze-sequence (begin-actions exp)))
+	((cond? exp) (analyze (cond->if exp)))
+	((let? exp) (analyze (let->combinations exp))) ;analyze let
+	((application? exp) (analyze-application exp))
+	(else
+	 (error "Unknown expression type -- ANALYZE" exp))))
+;; test let
+(eval '(let ((x 10)
+	     (y 12))
+	 (set! x (+ x x))
+	 (+ x y))
+      the-global-environment)
+;Value: 32
